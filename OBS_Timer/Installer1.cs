577d0e02a4f7ff01d112d6f8c5ad32869d9a67aa
Mike Bethany <mike@mikebethany.com>
2014-01-15 10:30:30 -0500

    Added License files

---
+++ OBS_Timer/Installer1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+
+
+namespace OBS_Timer
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            Process.Start(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\obs_timer.exe");
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+
+    }
+}
